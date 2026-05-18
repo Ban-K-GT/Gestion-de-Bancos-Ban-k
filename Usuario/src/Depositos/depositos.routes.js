f52bf454832a9627064bf7a001f661c726ab944e
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-05-18 10:20:38 -0600

    Se modificaron configuraciones de algunas rutas

--- Usuario/src/Depositos/depositos.routes.js
+++ Usuario/src/Depositos/depositos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { validateJWT } from '../../middlewares/validate-jwt.js';
+// import { validateJWT } from '../../middlewares/validate-jwt.js';
 import {
     getMisDepositos,
     getMisDepositosPorCuenta,
@@ -9,8 +9,8 @@ import {
 const router = Router();
 
 // Rutas de depósitos
-router.get('/mis-depositos', validateJWT, getMisDepositos); // Depósitos de todas las cuentas del usuario
-router.get('/mis-depositos/:numeroCuenta', validateJWT, getMisDepositosPorCuenta); // Mis depósitos por número de cuenta
-router.post('/', validateJWT, crearDeposito);
+router.get('/mis-depositos', getMisDepositos); // Depósitos de todas las cuentas del usuario
+router.get('/mis-depositos/:numeroCuenta', getMisDepositosPorCuenta); // Mis depósitos por número de cuenta
+router.post('/', crearDeposito);
 
 export default router;
